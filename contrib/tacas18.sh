a7424594b2d1459ab8c42edcb93d9e9df0ecbdc9
Kshitij Bansal <kshitij@cs.nyu.edu>
2018-01-07 08:14:35 -0500

    pyparsing

--- contrib/tacas18.sh
+++ contrib/tacas18.sh
@@ -12,6 +12,12 @@ python -c "import yaml" || {
   sudo apt-get install python-yaml
 } && echo "YAML already found or installed."
 
+python -c "import pyparsing" || {
+  echo "Installing pyparsing"
+  sudo apt-get install python-pyparsing
+} && echo "pyparsing already found or installed."
+
+
 [ -f  $CVC4 ] || {
   echo "Downloading CVC4."
   sudo wget -q 'http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.5-x86_64-linux-opt' -O $CVC4
